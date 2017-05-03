b838daad2f1e4d60523b915e75c49b5879492046
Tom Van Roy <tom.van.roy@outlook.com>
2017-05-03 14:15:30 +0200

    Einde Cursus

--- TDDCursusLibraryTest/WinstServiceTest.cs
+++ TDDCursusLibraryTest/WinstServiceTest.cs
@@ -1,26 +1,41 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TDDCursusLibrary;
+using Moq;
 
 namespace TDDCursusLibraryTest
 {
     [TestClass]
     public class WinstServiceTest
     {
-        private IOpbrengstDAO opbrengstDAO;
-        private IKostDAO kostDAO;
         private WinstService winstService;
+
+        private IKostDAO kostDAO;
+        private IOpbrengstDAO opbrengstDAO;
+        
+        private Mock<IKostDAO> mockKost;
+        private Mock<IOpbrengstDAO> mockOpbrengst;
+
         [TestInitialize]
         public void Initialize()
         {
-            opbrengstDAO = new OpbrengstDAOStub();
-            kostDAO = new KostDAOStub();
+            mockKost = new Mock<IKostDAO>();
+            mockOpbrengst = new Mock<IOpbrengstDAO>();
+
+            kostDAO = mockKost.Object;
+            opbrengstDAO = mockOpbrengst.Object;
+
+            mockKost.Setup(eenKostDAO => eenKostDAO.TotaleKost()).Returns(2500.25m);
+            mockOpbrengst.Setup(eenOpbrengstDAO => eenOpbrengstDAO.TotaleOpbrengst()).Returns(3000.75m);
+            
             winstService = new WinstService(opbrengstDAO, kostDAO);
         }
         [TestMethod]
         public void BerekenWinst()
         {
-            Assert.AreEqual(1084084.78m, winstService.Winst);
+            Assert.AreEqual(500.50m, winstService.Winst);
+            mockOpbrengst.Verify(eenOpbrengstDAO => eenOpbrengstDAO.TotaleOpbrengst());
+            mockKost.Verify(eenKostDAO => eenKostDAO.TotaleKost());
         }
     }
 }
